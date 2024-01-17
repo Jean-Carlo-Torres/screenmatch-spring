f47c59d42957b3e2b7542fc594eb0a53c02c1f88
Jean Carlo <jeansanto2002@gmail.com>
2024-01-17 17:25:32 -0300

    Criando um serviço para as séries

--- src/main/java/br/com/webapp/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/webapp/screenmatch/controller/SerieController.java
@@ -1,26 +1,21 @@
 package br.com.webapp.screenmatch.controller;
 
 import br.com.webapp.screenmatch.dto.SerieDTO;
-import br.com.webapp.screenmatch.models.Serie;
-import br.com.webapp.screenmatch.repository.SerieRepository;
+import br.com.webapp.screenmatch.services.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class SerieController{
 
     @Autowired
-    private SerieRepository repositorio;
+    private SerieService servico;
 
     @GetMapping("/series")
     public List<SerieDTO> obterSeries(){
-        return repositorio.findAll()
-                .stream()
-                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-                .collect(Collectors.toList());
+        return servico.obterTodasAsSeries();
     }
 }
---
+++ src/main/java/br/com/webapp/screenmatch/services/SerieService.java
@@ -0,0 +1,23 @@
+package br.com.webapp.screenmatch.services;
+
+import br.com.webapp.screenmatch.dto.SerieDTO;
+import br.com.webapp.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+
+    @Autowired
+    private SerieRepository repositorio;
+
+    public List<SerieDTO> obterTodasAsSeries() {
+        return repositorio.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+    }
+}
