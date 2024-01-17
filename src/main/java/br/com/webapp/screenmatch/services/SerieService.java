3d6715a9e770f12c437dcb342cdee05a799d857d
Jean Carlo <jeansanto2002@gmail.com>
2024-01-17 17:40:55 -0300

    Exibindo as top séries do banco

--- src/main/java/br/com/webapp/screenmatch/services/SerieService.java
+++ src/main/java/br/com/webapp/screenmatch/services/SerieService.java
@@ -1,6 +1,7 @@
 package br.com.webapp.screenmatch.services;
 
 import br.com.webapp.screenmatch.dto.SerieDTO;
+import br.com.webapp.screenmatch.models.Serie;
 import br.com.webapp.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,8 +16,15 @@ public class SerieService {
     private SerieRepository repositorio;
 
     public List<SerieDTO> obterTodasAsSeries() {
-        return repositorio.findAll()
-                .stream()
+        return converteDados(repositorio.findAll());
+    }
+
+    public List<SerieDTO> obterTop5Series() {
+        return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
+    }
+
+    private List<SerieDTO> converteDados(List<Serie> series) {
+        return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
                 .collect(Collectors.toList());
     }
